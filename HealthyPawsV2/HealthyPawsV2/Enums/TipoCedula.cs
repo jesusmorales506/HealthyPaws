8707a6decd6e93276640a7bad737b265ea9d4cd2
aleec24490 <acabalceta00165@ufide.ac.cr>
2024-10-04 18:09:28 -0600

    Add Script for DB Testing and Enum

---
+++ HealthyPawsV2/HealthyPawsV2/Enums/TipoCedula.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthyPawsV2.Enums
+{
+    using System.ComponentModel;
+    public enum TipoCedula
+    {
+        [Description("Nacional")]
+        NACIONAL,
+
+        [Description("Extranjero")]
+        EXTRANJERO,
+
+        [Description("Nacionalizado")]
+        NACIONALIZADO,
+    }
+}
