0219a866ba5afbf5f4a92b06f4d5e23e0d8ef341
aleec24490 <acabalceta00165@ufide.ac.cr>
2024-11-03 14:10:40 -0600

    Create User Fix

--- HealthyPawsV2/HealthyPawsV2/Controllers/UserController.cs
+++ HealthyPawsV2/HealthyPawsV2/Controllers/UserController.cs
@@ -20,8 +20,8 @@ public class UserController : Controller
 		_roleManager = roleManager;
 	}
 
-    [HttpGet]
-    public async Task<IActionResult> Index()
+	[HttpGet]
+	public async Task<IActionResult> Index()
 	{
 		var usuarios = await _userManager.Users.Where(p => p.status).ToListAsync();
 
@@ -33,18 +33,12 @@ public class UserController : Controller
 		{
 			ViewData["NoResultados"] = false;
 		}
-        ViewBag.Roles = await RolesUtils.GetAllRoles(_roleManager);
+		ViewBag.Roles = await RolesUtils.GetAllRoles(_roleManager);
 
-        return View(usuarios);
+		return View(usuarios);
 	}
-	
-    
 
-
-
-
-
-    public async Task<IActionResult> Details(string id)
+	public async Task<IActionResult> Details(string id)
 	{
 		if (id == null)
 		{
@@ -73,152 +67,147 @@ public class UserController : Controller
 	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 	[HttpPost]
 	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Create([Bind("name,surnames,lastConnection,idType,idNumber,phone1,phone2,phone3,Email")] ApplicationUser usuario, string provinceName, string cantonName, string districtName, string role)
+	public async Task<IActionResult> Create(
+		[Bind("name,surnames,lastConnection,idType,idNumber,phone1,phone2,phone3,Email")] ApplicationUser user,
+		string provinceName, string cantonName, string districtName, string role)
 	{
-		//We get the last connection of the user
-		usuario.lastConnection = DateTime.Now.Date + DateTime.Now.TimeOfDay;
-		usuario.status = true;
-		usuario.UserName = usuario.Email;
-		usuario.NormalizedUserName = usuario.Email.ToUpper();
-		usuario.NormalizedEmail = usuario.Email.ToUpper();
-		usuario.PhoneNumber = usuario.phone1;
+		// Set the roles for the view
+		ViewBag.Roles = await RolesUtils.GetAllRoles(_roleManager);
 
-		//This is a Validation to know if the user is adding the Name and Surnames
-		if (string.IsNullOrEmpty(usuario.name) || string.IsNullOrEmpty(usuario.surnames))
+		// Validate the model state
+		if (!ModelState.IsValid)
 		{
-			ModelState.AddModelError("", "Los campos de Nombre y Apellidos son obligatorios.");
-			return View(usuario);
+			return View(user); // Return with current model state errors
 		}
 
-		//Create password with some format
-		string password = char.ToUpper(usuario.name[0]) + usuario.name.Substring(1, 2).ToLower() + "." + char.ToLower(usuario.surnames[0]) + "123";
-		//hash the password
-		string hashedPassword = PasswordUtility.HashPassword(usuario, password);
-		usuario.PasswordHash = hashedPassword;
-
-		//Get logged user and assign it as loggedUser
-		var userIdentify = new ClaimsPrincipal(User.Identity);
-		var loggedUserTask = RolesUtils.GetLoggedUser(_userManager, userIdentify);
-		loggedUserTask.Wait();
-		var loggedUser = loggedUserTask.Result;
-
-		if (ModelState.IsValid)
+		// Validate required fields
+		if (string.IsNullOrEmpty(user.name) || string.IsNullOrEmpty(user.surnames))
 		{
-			var address = new Address
+			ModelState.AddModelError("", "Los campos de Nombre y Apellidos son obligatorios.");
+			return View(user);
+		}
+
+		// Assign last connection date
+		user.lastConnection = DateTime.Now;
+
+		// Set username and normalized values
+		user.UserName = user.Email;
+		user.NormalizedUserName = user.Email.ToUpper();
+		user.NormalizedEmail = user.Email.ToUpper();
+		user.PhoneNumber = user.phone1;
+
+		// Create password
+		string password = $"{char.ToUpper(user.name[0])}{user.name.Substring(1, 2).ToLower()}.{char.ToLower(user.surnames[0])}123";
+		user.PasswordHash = PasswordUtility.HashPassword(user, password);
+
+		// Create address
+		var address = new Address
+		{
+			province = provinceName,
+			canton = cantonName,
+			district = districtName
+		};
+
+		// Add address to context
+		_context.Addresses.Add(address);
+		await _context.SaveChangesAsync();
+
+		// Link address to user
+		user.addressId = address.AddressId;
+
+		// Create user
+		var result = await _userManager.CreateAsync(user, password);
+		if (!result.Succeeded)
+		{
+			foreach (var error in result.Errors)
 			{
-				province = provinceName,
-				canton = cantonName,
-				district = districtName
-			};
-
-			//Lines 131/132/133 are creating the addresses and adding them to btoh tables Addresses and Users.
-			_context.Addresses.Add(address);
-			await _context.SaveChangesAsync();
-			usuario.addressId = address.AddressId;
-
-			//Create user and add the password
-			var result = await _userManager.CreateAsync(usuario, password);
-
-			if (result.Succeeded)
-			{
-				//Assign role "User" to the new user if there is no indicated role previously.
-				if (role == null)
-				{
-					var resultRole = await _userManager.AddToRoleAsync(usuario, "User");
-
-					if (resultRole.Succeeded)
-					{
-						return RedirectToAction(nameof(Index));
-					}
-					else
-					{
-						foreach (var error in resultRole.Errors)
-						{
-							ModelState.AddModelError(string.Empty, error.Description);
-						}
-					}
-				}
+				ModelState.AddModelError(string.Empty, error.Description);
 			}
-			else
+			return View(user); // Return on error with model state
+		}
+
+		// Assign role
+		if (string.IsNullOrEmpty(role))
+		{
+			var resultRole = await _userManager.AddToRoleAsync(user, "User");
+			if (!resultRole.Succeeded)
 			{
-				foreach (var error in result.Errors)
+				foreach (var error in resultRole.Errors)
 				{
 					ModelState.AddModelError(string.Empty, error.Description);
 				}
+				return View(user); // Return on error with model state
 			}
 		}
 
-		//ViewData["IdRazaMascota"] = new SelectList(_context.RazaMascotas, "IdRazaMascota", "Nombre", mascota.IdRazaMascota);
-		//ViewData["Usuarios"] = new SelectList(_context.ApplicationUser, "Id", "Nombre", mascota.UsuarioCreacionId);
-		//ViewData["TiposMascota"] = new SelectList(_context.TipoMascotas, "IdTipoMascota", "Nombre", mascota.IdTipoMascota);
+		// Redirect to index if everything succeeded
+		return RedirectToAction(nameof(Index));
+	}
 
+
+	[HttpGet]
+	public async Task<IActionResult> Edit(string id)
+	{
+		var usuario = await _context.ApplicationUser.FindAsync(id);
+		if (usuario == null)
+		{
+			return NotFound();
+		}
 		return View(usuario);
 	}
 
 
-    [HttpGet]
-    public async Task<IActionResult> Edit(string id)
-    {
-        var usuario = await _context.ApplicationUser.FindAsync(id);
-        if (usuario == null)
-        {
-            return NotFound();
-        }
-        return View(usuario);
-    }
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Edit(string id, ApplicationUser usuario)
+	{
+		if (id != usuario.Id)
+		{
+			return NotFound();
+		}
+
+		if (ModelState.IsValid)
+		{
+			try
+			{
+				var userToUpdate = await _userManager.FindByIdAsync(id);
+				if (userToUpdate == null)
+				{
+					return NotFound();
+				}
+
+				userToUpdate.name = usuario.name;
+				userToUpdate.surnames = usuario.surnames;
+				userToUpdate.phone1 = usuario.phone1;
+				userToUpdate.phone2 = usuario.phone2;
+				userToUpdate.phone3 = usuario.phone3;
+				userToUpdate.Email = usuario.Email;
 
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(string id, ApplicationUser usuario)
-    {
-        if (id != usuario.Id)
-        {
-            return NotFound();
-        }
+				var result = await _userManager.UpdateAsync(userToUpdate);
+				if (!result.Succeeded)
+				{
 
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                var userToUpdate = await _userManager.FindByIdAsync(id);
-                if (userToUpdate == null)
-                {
-                    return NotFound();
-                }
-
-                userToUpdate.name = usuario.name;
-                userToUpdate.surnames = usuario.surnames;
-                userToUpdate.phone1 = usuario.phone1;
-                userToUpdate.phone2 = usuario.phone2;
-                userToUpdate.phone3 = usuario.phone3;
-                userToUpdate.Email = usuario.Email;
-
-     
-                var result = await _userManager.UpdateAsync(userToUpdate);
-                if (!result.Succeeded)
-                {
-
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
-                    return View(usuario);
-                }
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+					return View(usuario);
+				}
 
 
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-               
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(usuario);
-    }
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+
+			}
+			return RedirectToAction(nameof(Index));
+		}
+		return View(usuario);
+	}
 
 
-    public async Task<IActionResult> Delete(string id)
+	public async Task<IActionResult> Delete(string id)
 	{
 		if (id == null)
 		{
@@ -233,8 +222,8 @@ public class UserController : Controller
 
 		usuario.status = false;
 		await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-        //return View(usuario);
+		return RedirectToAction(nameof(Index));
+		//return View(usuario);
 	}
 
 	[HttpPost, ActionName("Delete")]
@@ -242,14 +231,14 @@ public class UserController : Controller
 	public async Task<IActionResult> DeleteConfirmed(string id)
 	{
 		var usuario = await _context.ApplicationUser.FindAsync(id);
-        if (usuario != null)
+		if (usuario != null)
 		{
 			usuario.status = false;
 		}
 		return RedirectToAction(nameof(Index));
 	}
 
-	
+
 }
 
 
