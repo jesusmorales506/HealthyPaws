6d7e89cc383ab6a92b6400305abe86197e30d36f
JafetPorras <Jporras50752@ufide.ac.cr>
2024-11-16 01:10:34 -0600

    Add_lookupBar_All
    This is a commit on behalf Kath.

--- HealthyPawsV2/HealthyPawsV2/Controllers/HomeController.cs
+++ HealthyPawsV2/HealthyPawsV2/Controllers/HomeController.cs
@@ -1,37 +1,79 @@
+using HealthyPawsV2.DAL;
 using HealthyPawsV2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HealthyPawsV2.Controllers
 {
+    public class SearchViewModel
+    {
+        public List<PetType> PetTypes { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+    }
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly HPContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(HPContext context, UserManager<ApplicationUser> userManager)
         {
-            _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
+
+        public async Task<IActionResult> Search(string GlobalSearch)
+        {
+            var petTypeQuery = _context.PetTypes.AsQueryable();
+            var usersQuery = _userManager.Users.AsQueryable();
+
+            var petBreedsQuery = _context.PetBreeds
+                .Include(r => r.PetType)
+                .Where(p => p.status)
+                .AsQueryable();
+
+            var appointments = _context.Appointments
+                .Include(a => a.PetFile)
+                .Include(a => a.owner)
+                .Include(a => a.vet)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(GlobalSearch))
+            {
+                petTypeQuery = petTypeQuery.Where(m => m.name.Contains(GlobalSearch));
+                usersQuery = usersQuery.Where(p => p.name.Contains(GlobalSearch));
+                petBreedsQuery = petBreedsQuery.Where(p => p.name.Contains(GlobalSearch) || p.PetType.name.Contains(GlobalSearch));
+                appointments = appointments.Where(a => a.owner.name.Contains(GlobalSearch) || a.vet.name.Contains(GlobalSearch) || a.PetFile.name.Contains(GlobalSearch));
+
+
+            }
+
+            var petTypeResult = await petTypeQuery.ToListAsync();
+            var usersResult = await usersQuery.ToListAsync();
+            var petBreedResult = await petBreedsQuery.ToListAsync();
+            var appointmentsResult = await appointments.ToListAsync();
+
+            ViewBag.PetTypes = petTypeResult;
+            ViewBag.Users = usersResult;
+            ViewBag.PetBreeds = petBreedsQuery;
+            ViewBag.Appointment = appointments;
+            ViewBag.NoResultadosPetTypes = petTypeResult.Count == 0;
+            ViewBag.NoResultadosUsers = usersResult.Count == 0;
+            ViewBag.NoResultadosPetBreeds = petBreedResult.Count == 0;
+            ViewBag.NoResultadosAppointment = appointmentsResult.Count == 0;
+
+            return View();
+        }
+
+
+
+
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult User()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
